924f0323ba7cfac3da3605c676f975780439a5d8
Alexander Wornast <40890820+tetracionist@users.noreply.github.com>
2024-12-24 12:59:47 +0000

    Releases/v0.1.0 (#4)
    * * Updated project metadata
    * Added uv
    * Added sqlfluff
    
    * remove wait.ts
    
    * update code
    
    * try again
    
    * added actions core
    
    * compile it properly
    
    * add pyproject.toml to dist
    
    * Update pyproject.toml
    
    * project-safe name
    
    * if dbt
    
    * Try again
    
    * try running dbt deps
    
    * use the right target
    
    * print password debug
    
    * recompile
    
    * Use relative paths
    
    * test with binary
    
    * add missing uv env
    
    * add dbt binary
    
    * Make sure we get indirect deps too
    
    * attempt to fix git diff
    
    * Use the base ref instead
    
    * Add baseref
    
    * use origin
    
    * Add reviewdog
    
    * add path
    
    * try again
    
    * install pipx globally
    
    * look at the binary folder
    
    * add await
    
    * testing reviewdog part
    
    * try again
    
    * Try without error
    
    * parse json file
    
    * catch sqlfluff error
    
    * try to get reviewdog to do something
    
    * correct newline character
    
    * use file filter mode
    
    * Fail on error
    
    * mvp

--- dist/index.js
+++ dist/index.js
@@ -25667,52 +25667,195 @@ var __importStar = (this && this.__importStar) || (function () {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.run = run;
 const core = __importStar(__nccwpck_require__(7484));
-const wait_1 = __nccwpck_require__(910);
+const exec = __importStar(__nccwpck_require__(5236));
+const path = __importStar(__nccwpck_require__(6928));
+const fs = __importStar(__nccwpck_require__(9896));
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
+const rdLintResultsFile = path.resolve('./lint-results.rdjsonl');
+async function getGitDiffFiles() {
+    const baseRef = process.env.GITHUB_BASE_REF || 'main';
+    console.log('Fetching main branch...');
+    await exec.exec('git', ['fetch', 'origin', `${baseRef}`]);
+    let stdout = '';
+    const options = {
+        listeners: {
+            stdout: (data) => {
+                stdout += data.toString();
+            }
+        }
+    };
+    // Run the git diff command
+    await exec.exec('git', [
+        'diff',
+        '--name-only',
+        '--diff-filter=ACMRU',
+        '--relative',
+        `origin/${process.env.GITHUB_BASE_REF}`,
+        '--',
+        '*.sql'
+    ], options);
+    // Process and return the list of files
+    return stdout
+        .trim()
+        .split('\n')
+        .filter(file => file.length > 0);
+}
+function resolveAndCheckPath(inputPath) {
+    if (!inputPath) {
+        return undefined; // Return undefined if no input is provided
+    }
+    const resolvedPath = path.resolve(inputPath);
+    // Return the resolved path only if it exists
+    return fs.existsSync(resolvedPath) ? resolvedPath : undefined;
+}
+async function setupUV() {
+    // Use UV to manage dependencies
+    try {
+        await exec.exec('python', ['-m', 'pip', 'install', '--user', 'pipx']);
+        await exec.exec('pipx', ['install', 'uv']);
+        console.log('Successfully installed uv.');
+    }
+    catch (error) {
+        console.error('Failed to install uv:', error);
+        throw error;
+    }
+}
+async function setupReviewDog() {
+    try {
+        console.log('Installing Reviewdog...');
+        await exec.exec('bash', [
+            '-c',
+            'curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b /usr/local/bin'
+        ]);
+        console.log('Reviewdog installation completed.');
+    }
+    catch (error) {
+        console.error('Failed to install Reviewdog:', error);
+        throw error;
+    }
+}
+async function setupDependencies(pyprojectPath) {
+    // Use UV to manage dependencies
+    try {
+        await exec.exec('uv', ['venv']);
+        await exec.exec('uv', ['pip', 'install', '-r', `${pyprojectPath}`]);
+        console.log('Successfully installed dependencies.');
+    }
+    catch (error) {
+        console.error('Failed to install dependencies:', error);
+        throw error;
+    }
+}
+async function processLintOutput(lintOutput) {
+    const rdjsonlines = lintOutput.flatMap(result => result.violations.map(violation => ({
+        message: violation.description,
+        location: {
+            path: `${result.filepath}`,
+            range: {
+                start: {
+                    line: violation.start_line_no,
+                    column: violation.start_line_pos
+                },
+                end: {
+                    line: violation.end_line_no,
+                    column: violation.end_line_pos
+                }
+            }
+        },
+        severity: 'ERROR'
+    })));
+    const rdjsonlContent = rdjsonlines
+        .map(line => JSON.stringify(line))
+        .join('\n');
+    fs.writeFileSync(rdLintResultsFile, rdjsonlContent, 'utf-8');
+}
+async function runReviewdog(rdjsonlFile) {
+    const rdFileContent = fs.readFileSync(rdjsonlFile);
+    await exec.exec('reviewdog', [
+        '-f=rdjsonl',
+        '-filter-mode=file',
+        '-reporter=github-pr-review',
+        '-fail-on-error=true'
+    ], {
+        input: rdFileContent
+    });
+}
 async function run() {
     try {
-        const ms = core.getInput('milliseconds');
-        // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-        core.debug(`Waiting ${ms} milliseconds ...`);
-        // Log the current timestamp, wait, then log the new timestamp
-        core.debug(new Date().toTimeString());
-        await (0, wait_1.wait)(parseInt(ms, 10));
-        core.debug(new Date().toTimeString());
-        // Set outputs for other workflow steps to use
-        core.setOutput('time', new Date().toTimeString());
+        console.log('Installing uv...');
+        await setupUV();
+        // check if there is a pyproject.toml we can use
+        let pyprojectPath = core.getInput('pyproject-path');
+        core.info(`Received pyproject.toml path: ${pyprojectPath}`);
+        if (!pyprojectPath) {
+            pyprojectPath = path.resolve(__dirname, 'pyproject.toml');
+            core.info(`No custom path provided. Using default pyproject.toml at: ${pyprojectPath}`);
+        }
+        // Install the dependencies using uv
+        await setupDependencies(pyprojectPath);
+        // check what type of project this is (e.g. Snowflake with dbt)
+        const dbtProjectDir = core.getInput('dbt-project-path') || undefined;
+        const dbtProfilesDir = core.getInput('dbt-profiles-path') || undefined;
+        const sqlfluffDialect = core.getInput('sqlfluff-dialect');
+        const sqlfluffTemplater = core.getInput('sqlfluff-templater');
+        const dbtExec = path.resolve('.venv/bin/dbt');
+        const sqlfluffExec = path.resolve('.venv/bin/sqlfluff');
+        const workspaceDir = path.resolve('.');
+        let lintResults = [];
+        if (dbtProjectDir) {
+            core.info(`DBT project directory set to: ${dbtProjectDir}`);
+            // change directory to dbt project directory
+            process.chdir(path.resolve(dbtProjectDir));
+            core.info(`Changed working directory to: ${dbtProjectDir}`);
+        }
+        if (dbtProfilesDir) {
+            core.info(`DBT profiles directory set to: ${dbtProfilesDir}`);
+            process.env.DBT_PROFILES_DIR = dbtProfilesDir;
+            process.env.DBT_TARGET = 'sqlfluff';
+            core.info(`DBT target set to: sqlfluff`);
+            await exec.exec(`${dbtExec}`, ['deps']);
+        }
+        // check for file changes
+        const filePaths = await getGitDiffFiles();
+        if (filePaths.length === 0) {
+            core.info('No SQL files changed.');
+            return;
+        }
+        try {
+            await exec.exec(`${sqlfluffExec}`, [
+                'lint',
+                '--dialect',
+                `${sqlfluffDialect}`,
+                '--templater',
+                `${sqlfluffTemplater}`,
+                ...filePaths,
+                '--format',
+                'json',
+                '--write-output',
+                'lint-results.json'
+            ]);
+        }
+        catch (error) { }
+        if (fs.existsSync('lint-results.json')) {
+            const content = fs.readFileSync('lint-results.json', 'utf-8');
+            let lintResults = JSON.parse(content);
+            console.log('Parsed Lint Results:', lintResults);
+            await processLintOutput(lintResults);
+        }
+        // process as rdjsonl
+        core.info('setup review dog');
+        await setupReviewDog();
+        core.info('running reviewdog');
+        await runReviewdog(rdLintResultsFile);
     }
     catch (error) {
         // Fail the workflow run if an error occurs
         if (error instanceof Error)
             core.setFailed(error.message);
     }
-}
-
-
-/***/ }),
-
-/***/ 910:
-/***/ ((__unused_webpack_module, exports) => {
-
-"use strict";
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.wait = wait;
-/**
- * Wait for a number of milliseconds.
- * @param milliseconds The number of milliseconds to wait.
- * @returns {Promise<string>} Resolves with 'done!' after the wait is over.
- */
-async function wait(milliseconds) {
-    return new Promise(resolve => {
-        if (isNaN(milliseconds)) {
-            throw new Error('milliseconds not a number');
-        }
-        setTimeout(() => resolve('done!'), milliseconds);
-    });
 }
 
 
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,264 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as exec from '@actions/exec'
+import * as path from 'path'
+import * as fs from 'fs'
 
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
+
+const rdLintResultsFile = path.resolve('./lint-results.rdjsonl')
+
+type Violation = {
+  description: string
+  start_line_no: number
+  start_line_pos: number
+  end_line_no: number
+  end_line_pos: number
+}
+
+type LintResult = {
+  filepath: string
+  violations: Violation[]
+}
+
+type RdjsonlLine = {
+  message: string
+  location: {
+    path: string
+    range: {
+      start: {
+        line: number
+        column: number
+      }
+      end: {
+        line: number
+        column: number
+      }
+    }
+  }
+  severity: string
+}
+
+async function getGitDiffFiles(): Promise<string[]> {
+  const baseRef = process.env.GITHUB_BASE_REF || 'main'
+
+  console.log('Fetching main branch...')
+  await exec.exec('git', ['fetch', 'origin', `${baseRef}`])
+
+  let stdout = ''
+
+  const options: exec.ExecOptions = {
+    listeners: {
+      stdout: (data: Buffer) => {
+        stdout += data.toString()
+      }
+    }
+  }
+
+  // Run the git diff command
+  await exec.exec(
+    'git',
+    [
+      'diff',
+      '--name-only',
+      '--diff-filter=ACMRU',
+      '--relative',
+      `origin/${process.env.GITHUB_BASE_REF}`,
+      '--',
+      '*.sql'
+    ],
+    options
+  )
+
+  // Process and return the list of files
+  return stdout
+    .trim()
+    .split('\n')
+    .filter(file => file.length > 0)
+}
+
+function resolveAndCheckPath(
+  inputPath: string | undefined
+): string | undefined {
+  if (!inputPath) {
+    return undefined // Return undefined if no input is provided
+  }
+
+  const resolvedPath = path.resolve(inputPath)
+
+  // Return the resolved path only if it exists
+  return fs.existsSync(resolvedPath) ? resolvedPath : undefined
+}
+
+async function setupUV(): Promise<void> {
+  // Use UV to manage dependencies
+  try {
+    await exec.exec('python', ['-m', 'pip', 'install', '--user', 'pipx'])
+    await exec.exec('pipx', ['install', 'uv'])
+    console.log('Successfully installed uv.')
+  } catch (error) {
+    console.error('Failed to install uv:', error)
+    throw error
+  }
+}
+
+async function setupReviewDog(): Promise<void> {
+  try {
+    console.log('Installing Reviewdog...')
+    await exec.exec('bash', [
+      '-c',
+      'curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b /usr/local/bin'
+    ])
+    console.log('Reviewdog installation completed.')
+  } catch (error) {
+    console.error('Failed to install Reviewdog:', error)
+    throw error
+  }
+}
+
+async function setupDependencies(
+  pyprojectPath: string | undefined
+): Promise<void> {
+  // Use UV to manage dependencies
+
+  try {
+    await exec.exec('uv', ['venv'])
+    await exec.exec('uv', ['pip', 'install', '-r', `${pyprojectPath}`])
+    console.log('Successfully installed dependencies.')
+  } catch (error) {
+    console.error('Failed to install dependencies:', error)
+    throw error
+  }
+}
+
+async function processLintOutput(lintOutput: LintResult[]) {
+  const rdjsonlines = lintOutput.flatMap(result =>
+    result.violations.map(violation => ({
+      message: violation.description,
+      location: {
+        path: `${result.filepath}`,
+        range: {
+          start: {
+            line: violation.start_line_no,
+            column: violation.start_line_pos
+          },
+          end: {
+            line: violation.end_line_no,
+            column: violation.end_line_pos
+          }
+        }
+      },
+      severity: 'ERROR'
+    }))
+  )
+
+  const rdjsonlContent = rdjsonlines
+    .map(line => JSON.stringify(line))
+    .join('\n')
+  fs.writeFileSync(rdLintResultsFile, rdjsonlContent, 'utf-8')
+}
+
+async function runReviewdog(rdjsonlFile: string): Promise<void> {
+  const rdFileContent = fs.readFileSync(rdjsonlFile)
+
+  await exec.exec(
+    'reviewdog',
+    [
+      '-f=rdjsonl',
+      '-filter-mode=file',
+      '-reporter=github-pr-review',
+      '-fail-on-error=true'
+    ],
+    {
+      input: rdFileContent
+    }
+  )
+}
+
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    console.log('Installing uv...')
+    await setupUV()
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    // check if there is a pyproject.toml we can use
+    let pyprojectPath = core.getInput('pyproject-path')
+    core.info(`Received pyproject.toml path: ${pyprojectPath}`)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    if (!pyprojectPath) {
+      pyprojectPath = path.resolve(__dirname, 'pyproject.toml')
+      core.info(
+        `No custom path provided. Using default pyproject.toml at: ${pyprojectPath}`
+      )
+    }
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    // Install the dependencies using uv
+    await setupDependencies(pyprojectPath)
+
+    // check what type of project this is (e.g. Snowflake with dbt)
+    const dbtProjectDir = core.getInput('dbt-project-path') || undefined
+    const dbtProfilesDir = core.getInput('dbt-profiles-path') || undefined
+    const sqlfluffDialect = core.getInput('sqlfluff-dialect')
+    const sqlfluffTemplater = core.getInput('sqlfluff-templater')
+    const dbtExec = path.resolve('.venv/bin/dbt')
+    const sqlfluffExec = path.resolve('.venv/bin/sqlfluff')
+    const workspaceDir = path.resolve('.')
+    let lintResults: LintResult[] = []
+
+    if (dbtProjectDir) {
+      core.info(`DBT project directory set to: ${dbtProjectDir}`)
+
+      // change directory to dbt project directory
+      process.chdir(path.resolve(dbtProjectDir))
+      core.info(`Changed working directory to: ${dbtProjectDir}`)
+    }
+
+    if (dbtProfilesDir) {
+      core.info(`DBT profiles directory set to: ${dbtProfilesDir}`)
+      process.env.DBT_PROFILES_DIR = dbtProfilesDir
+      process.env.DBT_TARGET = 'sqlfluff'
+      core.info(`DBT target set to: sqlfluff`)
+      await exec.exec(`${dbtExec}`, ['deps'])
+    }
+
+    // check for file changes
+    const filePaths = await getGitDiffFiles()
+
+    if (filePaths.length === 0) {
+      core.info('No SQL files changed.')
+      return
+    }
+    try {
+      await exec.exec(`${sqlfluffExec}`, [
+        'lint',
+        '--dialect',
+        `${sqlfluffDialect}`,
+        '--templater',
+        `${sqlfluffTemplater}`,
+        ...filePaths,
+        '--format',
+        'json',
+        '--write-output',
+        'lint-results.json'
+      ])
+    } catch (error) {}
+
+    if (fs.existsSync('lint-results.json')) {
+      const content = fs.readFileSync('lint-results.json', 'utf-8')
+      let lintResults = JSON.parse(content)
+      console.log('Parsed Lint Results:', lintResults)
+
+      await processLintOutput(lintResults)
+    }
+
+    // process as rdjsonl
+    core.info('setup review dog')
+    await setupReviewDog()
+
+    core.info('running reviewdog')
+
+    await runReviewdog(rdLintResultsFile)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
